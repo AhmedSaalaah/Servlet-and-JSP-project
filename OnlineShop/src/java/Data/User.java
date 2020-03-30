267bb8a5081340503dfa5fea6f69606bc9037ce1
AhmedAlsum <ahmedalsum2@gmail.com>
2020-03-30 21:09:16 +0200

    Define Data classes and Connection with DB

---
+++ OnlineShop/src/java/Data/User.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author ahmed
+ */
+public class User {
+    
+    public int id;
+    public String username;
+    public String pass;
+    public String email;
+    public boolean isadmin;
+    public int credit_limit;
+    
+}
