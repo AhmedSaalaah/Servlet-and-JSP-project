d79c3a492f5fea1d492d4d8d3a3a60de4b5d33a5
AhmedAlsum <ahmedalsum2@gmail.com>
2020-04-01 20:03:55 +0200

    Function Register

--- OnlineShop/src/java/Database/ConnectDB.java
+++ OnlineShop/src/java/Database/ConnectDB.java
@@ -36,43 +36,52 @@ public class ConnectDB {
     public Boolean checkLogin(User usr) throws SQLException, ClassNotFoundException {
         connect();
         String sql = "select * from users where username = ? and password = ?";
-
         pst = con.prepareStatement(sql);
         pst.setString(1, usr.username);
         pst.setString(2, usr.pass);
         rs = pst.executeQuery();
         if (rs.next()) {
-
             return true;
         }
-
         return false;
-
     }
 
     public Boolean isadmin(User usr) throws SQLException, ClassNotFoundException {
         connect();
         String sql = "select * from users where username = ? and is_admin = true";
-        
         pst = con.prepareStatement(sql);
         pst.setString(1, usr.username);
         rs = pst.executeQuery();
         if (rs.next()) {
-           
             return true;
         }
-
         return false;
+    }
 
+    public int registerUser(User usr) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "insert into users (username,password,email,credit_limit) values (?,?,?,?)";
+        pst = con.prepareStatement(sql);
+        System.out.println("inserted");
+        pst.setString(1, usr.username);
+        pst.setString(2, usr.pass);
+        pst.setString(3, usr.email);
+        pst.setInt(4, usr.credit_limit);
+         int rs = pst.executeUpdate();
+        System.out.println(rs);
+       
+        return rs;
     }
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//        ConnectDB c = new ConnectDB();
-//        c.connect();
-//        User u = new User();
-//        u.username = "admin";
-//        u.pass = "admin";
-//        c.checkLogin(u);
+        ConnectDB c = new ConnectDB();
+        c.connect();
+        User u = new User();
+        u.username = "admin";
+        u.pass = "admin";
+        u.email = "admin";
+        u.credit_limit = 1000;
+        c.registerUser(u);
     }
 
 }
--- OnlineShop/src/java/Servlets/Register.java
+++ OnlineShop/src/java/Servlets/Register.java
@@ -7,21 +7,21 @@ package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import Database.*;
 import Data.*;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author ahmed
  */
-public class Login extends HttpServlet {
+public class Register extends HttpServlet {
 
     ConnectDB conn = new ConnectDB();
     User usr = new User();
@@ -33,24 +33,17 @@ public class Login extends HttpServlet {
             PrintWriter out = resp.getWriter();
             usr.username = req.getParameter("username");
             usr.pass = req.getParameter("pass");
-            
-            if (conn.checkLogin(usr)) {
-                if (conn.isadmin(usr)) {
-                    
-                    out.print("admin");
-                    
-                } else {
-                    out.print("user");
-                    resp.sendRedirect("index.html");
-                }
+            usr.email = req.getParameter("email");
+            usr.credit_limit = Integer.parseInt(req.getParameter("credit"));
 
+            if (conn.registerUser(usr) ==1) {
+
+                resp.sendRedirect("Login.html");
             } else {
-
-                out.print("username doesnot match");
+                out.print("Data Requried");
             }
-            } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
 }
